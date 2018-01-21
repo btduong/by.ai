9eb4e9b07c5ad51acfe17907d48242aee13e5a9a
Binh Duong <bduong@pushtechnology.com>
2018-01-21 23:10:56 +0000

    Enforc enter and exit for each state

--- src/State.ts
+++ src/State.ts
@@ -1,5 +1,7 @@
 import { BaseGameEntity } from "./BaseEntity";
 
 export interface State {
+    enter(entity: BaseGameEntity): void;
     execute(entity: BaseGameEntity): void;
+    exit(entity: BaseGameEntity): void;
 }
